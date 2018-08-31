4e8aaa6e662e87539601d1bcd0f794c2857e9bff
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-08-31 10:22:18 -0400

    addded table view and code

--- GroceryList/GroceryList/ViewController.swift
+++ GroceryList/GroceryList/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GroceryViewController: UITableViewController {
 
+     var groceryList = ["Eggs", "Milk", "Cheese", "Corn", "Pizza", "Bread", "Ice Cream", "Garbage Bags", "Magazines", "Noodles", "Yogurt", "Pizza Rolls", "Whiskey", "Single Grapefruit"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceryList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroceryListItem", for: indexPath)
+        
+        cell.textLabel?.text = groceryList[indexPath.row]
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            groceryList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        }
+    }
 }
+    
+
+
 
